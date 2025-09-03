f4bdf325c41a3111e3d22be28b4d5aef8a700de3
Maya Thompson <majsant96@hotmail.com>
2025-09-03 13:55:33 +0200

    update mobile menu html and add hamburger icon

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById("mobile-menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+toggleBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("d-none");
+});
