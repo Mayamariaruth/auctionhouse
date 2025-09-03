1bea19cf1d869c995b3a55872fa256e55342281d
Maya Thompson <majsant96@hotmail.com>
2025-09-03 14:09:44 +0200

    add styling to mobile menu and add toggle js code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const toggleBtn = document.getElementById("mobile-menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
+const menuIcon = document.getElementById("menu-icon");
 
 toggleBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("d-none");
+  menuIcon.classList.toggle("fa-bars");
+  menuIcon.classList.toggle("fa-xmark");
 });
