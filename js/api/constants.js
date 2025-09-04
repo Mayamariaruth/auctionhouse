7df3494aa9d6bca6474e86887e42f44725924a15
Maya Thompson <majsant96@hotmail.com>
2025-09-04 20:59:53 +0200

    create constants.js file for url links

---
+++ js/api/constants.js
@@ -0,0 +1,11 @@
+export const API_BASE = "https://v2.api.noroff.dev";
+
+// Auth endpoints
+export const API_AUTH = `${API_BASE}/auth`;
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+// Auction endpoints
+export const API_AUCTIONS = `${API_BASE}/auction`;
+export const API_AUCTIONS_LISTINGS = `${API_AUCTIONS}/listings`;
+export const API_AUCTIONS_PROFILES = `${API_AUCTIONS}/profiles`;
